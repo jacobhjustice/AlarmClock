c20fa2ee476abf3cd87a8d3ddf71a68b65c2642d
jacobhjustice <jacobhj1997@gmail.com>
2021-01-10 22:40:13 -0600

    Stubs and examples

--- clock/clock.ino
+++ clock/clock.ino
@@ -9,25 +9,57 @@
 #define PIN_LCD_DATA_3 9
 #define PIN_LCD_DATA_4 10
 
-bool _isEnabled = false;
+#define VIEW_TIME 1
+#define VIEW_EDIT_ALARM_1 2
+#define VIEW_EDIT_ALARM_2 3
+#define VIEW_EDIT_ALARM_2 4
+#define VIEW_EDIT_ALARM_2 5
+
+bool _isEditingTime;
+int _activeView;
+
 RTC_DS3231 rtc;
 LiquidCrystal lcd(PIN_LCD_RS, PIN_LCD_ENABLE, PIN_LCD_DATA_1, PIN_LCD_DATA_2, PIN_LCD_DATA_3, PIN_LCD_DATA_4);
 
-void setup() {
+void setup () {  
   Serial.begin(9600);
   lcd.begin(16, 2);
-  
-    // Clears the LCD screen
-    lcd.clear();
+  lcd.clear();
+
+  _isEditingTime = false;
+  _activeView = VIEW_TIME;
 }
 
-void loop() {
-  lcd.setCursor(0, 0);
-  lcd.print(" Hello world!");
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // Print a message to the LCD.
-  lcd.print(" LCD Tutorial");
+void loop () {
 
 }
+
+void display () {
+
+}
+
+void displayEditAlarm (int alarmValue) {
+
+}
+
+void checkCurrentTime () {
+  // Get Current Time
+  // Check against all active alarms
+}
+
+struct Alarm {
+    int hour;
+    int minute;
+    Day[] days;
+    bool isActive;
+}
+
+enum Day {
+    Monday = 1, 
+    Tuesday = 2,
+    Wednesday = 4,
+    Thursday = 8,
+    Friday = 16,
+    Saturday = 32,
+    Sunday = 64
+};
